739793d55d18eb5dac24671b327642ab3453e9e6
Glenn985 <66416139+Glenn985@users.noreply.github.com>
2024-02-26 10:33:02 +0800

    wheather app by zipcode.py

--- wheather app by zipcode.py
+++ wheather app by zipcode.py
@@ -6,21 +6,15 @@ root = Tk()
 root.title("Air Quality")
 root.geometry("500x200")
 
-
 def fetch_data():
     try:
-        
         zip_code = int(entry.get())
-        
-     I
         api_request = requests.get(f"https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode={zip_code}&distance=25&API_KEY=0862CA47-FC72-4A3E-8BFE-DD5B6CD6CCBB")
         api = json.loads(api_request.content)
         
-       
         city = api[0]['ReportingArea']
         quality = api[0]['AQI']
         category = api[0]['Category']['Name']
-        
         
         if category == "Good":
             weather_color = "Green"
@@ -32,20 +26,17 @@ def fetch_data():
         display_text = f"City: {city}\nAir Quality Index (AQI): {quality}\nCategory: {category}"
         output_label.config(text=display_text, background=weather_color)
     except Exception as e:
-      
         output_label.config(text="Error.....")
-
 
 entry = Entry(root)
 entry.pack()
 
-
 button = Button(root, text="Fetch Data", command=fetch_data)
 button.pack()
-
 
 output_label = Label(root, text="", font=("Arial", 12))
 output_label.pack()
 
 root.mainloop()
+
 
